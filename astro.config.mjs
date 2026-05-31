7b94b5602404de94232cc8585413c4adf38f4740
Dorian <gaurondorian@gmail.com>
2026-05-31 11:43:19 -0400

    👷 Set correct url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://Michelprogram.github.io",
+  base: "/cat-sitting/",
   vite: {
     plugins: [tailwindcss()],
   },
